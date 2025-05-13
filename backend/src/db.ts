29d276a794022d8cc21ab341558bd80b34a84def
Hugo Piat <h.piat@outlook.fr>
2025-05-13 15:27:10 +0200

    Added env variables for mongo db (all ones)

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,7 +3,9 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "mongodb",
   url: process.env.MONGO_URL,
-  database: "job-jarvis-dev",
+  database: process.env.MONGO_DB,
+  username: process.env.MONGO_USER,
+  password: process.env.MONGO_PASSWORD,
   synchronize: true, // ⚠️ dev only
   logging: true,
   entities: ["src/models/**/*.ts"],
