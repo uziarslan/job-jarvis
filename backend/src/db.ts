3a22589219c4690ee0b6fb57a411054ba0dd1185
Hugo Piat <h.piat@outlook.fr>
2025-05-13 15:11:26 +0200

    Fixed MONGO_URL for railway

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
-  host: "${{ MongoDB.MONGO_URL }}",
+  host: process.env.MONGO_URL,
   port: 27017,
   database: "job-jarvis-dev",
   synchronize: true, // ⚠️ dev only
