f7e69138832496f7765b2e770fb182061e835b81
Hugo Piat <h.piat@outlook.fr>
2025-05-13 17:10:38 +0200

    Definitively fixed envs management (localhost / railway)

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,13 +1,15 @@
 import { DataSource } from "typeorm";
+import { AIProfile } from "./models/AIProfile";
+import { Template } from "./models/Template";
+import { User } from "./models/User";
 
 export const AppDataSource = new DataSource({
   type: "mongodb",
   url: process.env.MONGO_URL,
   synchronize: true,
   logging: true,
-  entities: [
-    process.env.NODE_ENV === "dev"
-      ? "dist/models/**/*.ts"
-      : "src/models/**/*.js",
-  ],
+  entities: [AIProfile, Template, User],
+  extra: {
+    dbName: "job-jarvis-dev",
+  },
 });
